9123b3ee353a60cf6debd721a5d69a1198a0d7ef
Adarshr1927 <adarshprofessionals1927@gmail.com>
2026-07-28 04:16:46 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,53 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export const meta = () => ([
+  { title: 'Resumind | Auth' },
+  { name: 'description', content: 'Log into your account' },
+])
+
+const Auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split('next=')[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next])
+
+  return (
+      <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className="gradient-border shadow-lg">
+          <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+            <div className="flex flex-col items-center gap-2 text-center">
+              <h1>Welcome</h1>
+              <h2>Log In to Continue Your Job Journey</h2>
+            </div>
+            <div>
+              {isLoading ? (
+                  <button className="auth-button animate-pulse">
+                    <p>Signing you in...</p>
+                  </button>
+              ) : (
+                  <>
+                    {auth.isAuthenticated ? (
+                        <button className="auth-button" onClick={auth.signOut}>
+                          <p>Log Out</p>
+                        </button>
+                    ) : (
+                        <button className="auth-button" onClick={auth.signIn}>
+                          <p>Log In</p>
+                        </button>
+                    )}
+                  </>
+              )}
+            </div>
+          </section>
+        </div>
+      </main>
+  )
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+export default Auth
